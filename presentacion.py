dce3f2f3847ffe5b0538b9f75f433221e58ab4ae
emolas4161-commits <emolas4161@gmail.com>
2026-07-09 16:41:08 -0500

    Add files via upload

---
+++ presentacion.py
@@ -0,0 +1,12 @@
+# presentacion.py
+# Mi primera contribución a GitHub
+nombre = "Salome Morales Muñoz"
+grado = "11-01"
+colegio = "IE Francisco Jose de Caldas"
+lenguaje = "Python 🐍"
+print("=" * 45)
+print(f" Hola GitHub! Soy {nombre}")
+print(f" Grado: {grado} | Institución: {colegio}")
+print(f" Aprendiendo: {lenguaje}")
+print(f" ¡Este es mi primer commit! ")
+print("=" * 45)
